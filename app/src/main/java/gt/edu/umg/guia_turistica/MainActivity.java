8315a4a9f30d56817293bb4ae1023c31a9c9de02
Melki <mortizm28@miumg.edu.gt>
2024-10-29 19:20:22 -0600

    Ya esta finalizado todo

---
+++ app/src/main/java/gt/edu/umg/guia_turistica/MainActivity.java
@@ -0,0 +1,115 @@
+package gt.edu.umg.guia_turistica;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+
+import gt.edu.umg.guia_turistica.database.DatabaseHelper;
+import gt.edu.umg.guia_turistica.galeria.EasterEggActivity;
+import gt.edu.umg.guia_turistica.galeria.GaleriaActivity;
+import gt.edu.umg.guia_turistica.models.PhotoItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnfoto;
+    private TextView tvSaludo, lastPhotoDescription, lastPhotoDate;
+    private ImageView lastPhotoImage;
+    private DatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        initializeViews();
+        setupClickListeners();
+        dbHelper = new DatabaseHelper(this);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void initializeViews() {
+        tvSaludo = findViewById(R.id.tvSaludo);
+        btnfoto = findViewById(R.id.btnfoto);
+        lastPhotoImage = findViewById(R.id.lastPhotoImage);
+        lastPhotoDescription = findViewById(R.id.lastPhotoDescription);
+        lastPhotoDate = findViewById(R.id.lastPhotoDate);
+    }
+
+    private void setupClickListeners() {
+
+        btnfoto.setOnClickListener(v -> {
+            Intent intent = new Intent(this, GaleriaActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    public void openEasterEgg(View view) {
+        Intent intent = new Intent(this, EasterEggActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+    public void showCredits(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Créditos");
+        builder.setMessage("Aplicación desarrollada por:\n- Melki Bladimir Ortiz Martinez\n- Tulio Rene Alejandro Quintana Amezquita\n- Luke Skywalker");
+        builder.setPositiveButton("OK", null);
+        builder.show();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadLastPhoto();
+    }
+
+    private void loadLastPhoto() {
+        List<PhotoItem> photos = dbHelper.getAllPhotos();
+        if (!photos.isEmpty()) {
+            PhotoItem lastPhoto = photos.get(0); // La primera foto es la más reciente
+
+            // Cargar la imagen
+            try {
+                lastPhotoImage.setImageURI(Uri.parse(lastPhoto.getUri()));
+            } catch (Exception e) {
+                lastPhotoImage.setImageResource(R.drawable.kirby);
+            }
+
+            // Mostrar la descripción
+            lastPhotoDescription.setText(lastPhoto.getDescription());
+
+            // Mostrar la fecha
+            lastPhotoDate.setText("Tomada el " + lastPhoto.getDate());
+
+            // Hacer visible el CardView
+            findViewById(R.id.lastPhotoCard).setVisibility(View.VISIBLE);
+        } else {
+            // Si no hay fotos, ocultar el CardView
+            findViewById(R.id.lastPhotoCard).setVisibility(View.GONE);
+        }
+    }
+}
---
+++ app/src/main/java/gt/edu/umg/guia_turistica/galeria/EasterEggActivity.java
@@ -0,0 +1,15 @@
+package gt.edu.umg.guia_turistica.galeria;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import gt.edu.umg.guia_turistica.R;
+
+
+public class EasterEggActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easter_egg);
+    }
+}
